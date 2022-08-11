1f70ddc006c0b270d38489cc85c03a26f0e6f595
Lakshya291 <lakshya0417.be20@chitkara.edu.in>
2022-08-11 20:58:59 +0530

    Final Commit for the project

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ xhr.onreadystatechange = function () {
 		// console.log(response);
 
 		let template = "";
-		// response.length
-		for (let i = 0; i < 20; i++) {
+
+		for (let i = 0; i < response.length; i++) {
 			template += `
 			<div class="col-lg-4 mb-5">
 				<div class="card m-5" style="width: 18rem">
 					<img src="${response[i].thumbnailUrl}" loading="lazy" class="card-img-top py-3 px-2" alt="..." />
 					<div class="card-body">
 						<h5 class="card-title" style="max-height:50px; overflow-y:hidden; text-overflow: ellipsis;"> ${response[i].id}. ${response[i].title} </h5>
-						<a href="${response[i].url}" id="show" class="btn btn-primary">See Full Photo</a>
+						<a href="#"  class="btn btn-primary" onclick="showM('${response[i].url}')">See Full Photo</a>
 					</div>
 				</div>	
 			</div>	
@@ -34,24 +34,12 @@ xhr.send();
 // Modal scripting
 const modal = document.querySelector("#modal");
 function showM(BigUrl) {
-	modal.innerHTML = `<img src=${BigUrl} alt="Err... Sorry, Couldn't Load">`;
+	modal.style.display = "block";
+	modal.innerHTML = `<img src=${BigUrl} alt="Err... Sorry, Couldn't Load" class="container p-3 bg-light" style="border:2px solid black">`;
 }
 
 addEventListener("keydown", (e) => {
 	if (e.key == "Escape") {
 		modal.style.display = "none";
 	}
-});
-
-document.querySelector("#show").addEventListener("click", () => {
-	const template = `
-			<div class="card" style="width: 18rem;">
-			<img src="..." class="card-img-top" alt="...">
-			<div class="card-body">
-			<h5 class="card-title">Card title</h5>
-			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
-			</div>
-		</div>
-	`;
 });
